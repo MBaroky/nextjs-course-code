c38c0f2f996e07e51226aac185a880ad4f964ffd
Mohamed Gamal El Baroky <m.baroky@gmail.com>
2021-10-21 02:04:25 +0200

    first commit

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,16 @@
+const MeetupDetails = () => {
+  return (
+    <>
+      <img
+        style={{ maxWidth: "100%" }}
+        src='https://images.unsplash.com/photo-1556905200-279565513a2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80'
+        alt=''
+      />
+      <h1>A First Meetup</h1>
+      <address>address goes here</address>
+      <p>The Meetup description</p>
+    </>
+  );
+};
+
+export default MeetupDetails;
---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://images.unsplash.com/photo-1556905200-279565513a2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+    address: "5 some st, 12345 city country",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://images.unsplash.com/photo-1556905200-279565513a2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+    address: "10 some st, 12345 city country",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
